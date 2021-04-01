3168e5027759c99659f00431a0758794564956f4
Begzod Bakhriddinov <beka083bakhriddinov@gmail.com>
2021-04-01 19:56:18 +0500

    functions and guard statement

--- MyPlayground.playground/Pages/Swift's Complex Parts.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Swift's Complex Parts.xcplaygroundpage/Contents.swift
@@ -55,3 +55,58 @@ if let myTextView = myScrollView as? UITextView {
 } else {
     print("Type cast to UITextView failed")
 }
+//: # Control Flow
+//: The guard statement:
+func multiplyByTen(value: Int?) {
+    
+    guard let number = value else {
+        print("value is nil")
+        return
+    }
+    
+    print("\(number * 10)")
+}
+multiplyByTen(value: 10)
+//: # Functions, Methods and Closures
+//: functions:
+func buildMessage(user name: String = "Customer", usercount count: Int) -> String {
+    return "\(name), you are customer number \(count)"
+}
+let message = buildMessage(usercount: 56)
+//: returning multiple values:
+func sizeConverter(_ length: Float) -> (yards: Float, centimeters: Float, meters: Float) {
+    let yards = length * 0.0277778
+    let centimeters = length * 2.54
+    let meters = length * 0.0254
+    
+    return (yards, centimeters, meters)
+}
+let lengthTuple = sizeConverter(20.0)
+//: in-out parameters:
+func doubleValue(_ value: inout Int) -> Int {
+    value += value
+    return value
+}
+var a = 45
+doubleValue(&a)
+print(a)
+//: functions as parameters:
+func inchesToFeet(_ inches: Float) -> Float {
+    return inches * 0.083333
+}
+func inchesToYards(_ inches: Float) -> Float {
+    return inches * 0.0277778
+}
+let toFeet = inchesToFeet
+let toYards = inchesToYards
+
+func outputConversion(_ converterFunc: (Float) -> Float, value: Float) {
+    print("Result of conversion is \(converterFunc(value))")
+}
+outputConversion(toFeet, value: 10.0)
+outputConversion(toYards, value: 10.0)
+
+func decideFunc(_ feet: Bool) -> (Float) -> Float {
+    return feet ? toFeet : toYards
+}
+decideFunc(false)

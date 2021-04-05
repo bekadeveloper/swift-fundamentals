53d337d3a34ebe0dd50cf33cae1d1d6318a88f02
Begzod Bakhriddinov <bekadeveloper@icloud.com>
2021-04-05 10:48:05 +0500

    demo

---
+++ Demo/Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Begzod Bakhriddinov on 05/04/21.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

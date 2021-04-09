f138796e2494bde76839b34c6db4cfc1ef2c6f90
Begzod Bakhriddinov <bekadeveloper@icloud.com>
2021-04-09 11:21:57 +0500

    created demo project

---
+++ SwiftUI-Essentials/DemoProject/Shared/DemoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoProjectApp.swift
+//  Shared
+//
+//  Created by Begzod Bakhriddinov on 09/04/21.
+//
+
+import SwiftUI
+
+@main
+struct DemoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

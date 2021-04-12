70ad28849907dad154a7a9e997d5e84d03847ca3
Begzod Bakhriddinov <bekadeveloper@icloud.com>
2021-04-12 10:18:53 +0500

    renamed playground and created LifecycleDemo project

---
+++ SwiftUI-Essentials/LifecycleDemo/Shared/LifecycleDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifecycleDemoApp.swift
+//  Shared
+//
+//  Created by Begzod Bakhriddinov on 12/04/21.
+//
+
+import SwiftUI
+
+@main
+struct LifecycleDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
